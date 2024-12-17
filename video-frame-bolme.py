fcfb0cd20bef6dd2203f1e3110edf44c88128b45
Aleyna Melisa Dursunoğulları <122611251+AleynaMelisaDursunogullari@users.noreply.github.com>
2024-12-17 11:52:10 +0300

    Update video-frame-bolme.py

--- video-frame-bolme.py
+++ video-frame-bolme.py
@@ -7,13 +7,13 @@ This is a temporary script file.
 
 import cv2
 
-cap=cv2.VideoCapture(r"C:\Users\Melisa\Desktop\video\WhatsApp Video 2024-06-21 at 22.40.10 (1) (1).mp4")
+cap=cv2.VideoCapture(r"C:\Users\Desktop\video\WhatsApp Video-2024.mp4")
 
 sayac=10000
 
 while True:
     success, img=cap.read()
-    outfile =r'C:\Users\Melisa\Desktop\video\Resimler\resim_%s.jpg' % (sayac)
+    outfile =r'C:\Users\Desktop\video\Resimler\resim_%s.jpg' % (sayac)
     sayac+=1
     print(outfile)
     cv2.imwrite(outfile, img)
